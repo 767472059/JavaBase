e6dde06c33c7dbbd5319338414ff6a9159ef4896
shizeming <767472059@qq.com>
2018-10-23 21:52:09 +0800

    解决

--- JavaCore/src/com/ddb/javacore/ood5/INoteBookImpl.java
+++ JavaCore/src/com/ddb/javacore/ood5/INoteBookImpl.java
@@ -2,18 +2,15 @@ package com.ddb.javacore.ood5;
 
 public class INoteBookImpl implements INoteBook {
 
-	@Override
 	public void startOS() {
 		System.out.println("我是另一个INoteBook接口的实现类，我正在启动系统！");
 
 	}
 
-	@Override
 	public void printSelfName() {
 		System.out.println("我是另一个INoteBook接口的实现类，我的实现类的名字是： " + this.getClass());
 	}
 
-	@Override
 	public void usePs() {
 		System.out.println("我是另一个INoteBook接口的实现类，我也可以使用PS！ ");
 
