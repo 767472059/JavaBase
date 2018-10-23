829e0fe606ef2364e07a6aee0cf3f80e69b1233e
shizeming <767472059@qq.com>
2018-10-23 22:08:12 +0800

    测试提交

--- JavaCore/src/com/ddb/javacore/ood5/MPhone.java
+++ JavaCore/src/com/ddb/javacore/ood5/MPhone.java
@@ -2,13 +2,11 @@ package com.ddb.javacore.ood5;
 
 public class MPhone implements IiPhone {
 
-	@Override
 	public void startOS() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void printSelfName() {
 		// TODO Auto-generated method stub
 
--- JavaCore/src/com/ddb/javacore/ood5/TPhone.java
+++ JavaCore/src/com/ddb/javacore/ood5/TPhone.java
@@ -2,12 +2,10 @@ package com.ddb.javacore.ood5;
 
 public class TPhone implements IiPhone {
 
-	@Override
 	public void startOS() {
 		System.out.println("我是锤子手机，我实现了IiPhone接口的 startOS方法！");
 	}
 
-	@Override
 	public void printSelfName() {
 		System.out.println("我是锤子手机，我实现了IiPhone接口的 printSelfName方法！");
 	}
